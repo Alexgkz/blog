fd6d55eb7fc1bafe1c5835483a42e20e32ad104e
Alexgkz <alexgkz@mail.ru>
2021-09-01 13:53:21 +0300

    lesson BLOG after test commit

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 # blog/views.py
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView # новое
  
 from .models import Post
  
@@ -7,3 +7,7 @@ from .models import Post
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+    
+class BlogDetailView(DetailView): # новое
+    model = Post
+    template_name = 'post_detail.html'
