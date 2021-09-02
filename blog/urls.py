341a15eb4aad2faae525589f1e9dafe9ab5fea6a
Alexgkz <alexgkz@mail.ru>
2021-09-02 15:14:18 +0300

    CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,18 @@
 # blog/urls.py
 from django.urls import path
  
-from .views import BlogListView, BlogDetailView  # новое изменение
+from .views import (
+            BlogListView, 
+            BlogDetailView, 
+            BlogCreateView,
+            BlogUpdateView,# new  
+            BlogDeleteView, # Импортируем представление
+)
  
 urlpatterns = [
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'), # Создаем новый маршрут
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'), # Новый маршрут
+    path('post/new/', BlogCreateView.as_view(), name='post_new'), # new
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'), # новое изменение
     path('', BlogListView.as_view(), name='home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 # blog/views.py
 from django.views.generic import ListView, DetailView # новое
- 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView # новое изменение
+from django.urls import reverse_lazy 
 from .models import Post
  
  
@@ -11,3 +12,19 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView): # новое
     model = Post
     template_name = 'post_detail.html'
+
+    
+class BlogCreateView(CreateView): # новое изменение
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+    
+class BlogUpdateView(UpdateView): # Новый класс
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+    
+class BlogDeleteView(DeleteView): # Создание нового класса
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
